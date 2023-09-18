40f66e099a89f1fa3349e34efd6f5e86fb28397a
ksa607 <karine.samyn@hogent.be>
2023-09-18 15:58:09 +0200

    Add connection to database

---
+++ src/data/index.js
@@ -0,0 +1,64 @@
+const knex = require('knex');
+const { getLogger } = require('../core/logging');
+
+const config = require('config');
+
+const NODE_ENV = config.get('env');
+const isDevelopment = NODE_ENV === 'development';
+
+const DATABASE_CLIENT = config.get('database.client');
+const DATABASE_NAME = config.get('database.name');
+const DATABASE_HOST = config.get('database.host');
+const DATABASE_PORT = config.get('database.port');
+const DATABASE_USERNAME = config.get('database.username');
+const DATABASE_PASSWORD = config.get('database.password');
+
+let knexInstance;
+
+async function initializeData() {
+  const logger = getLogger();
+  logger.info('Initializing connection to the database');
+
+  const knexOptions = {
+    client: DATABASE_CLIENT,
+    connection: {
+      host: DATABASE_HOST,
+      port: DATABASE_PORT,
+      database: DATABASE_NAME,
+      user: DATABASE_USERNAME,
+      password: DATABASE_PASSWORD,
+      insecureAuth: isDevelopment,
+    },
+  };
+
+  knexInstance = knex(knexOptions);
+
+  try {
+    await knexInstance.raw('SELECT 1+1 AS result');
+  } catch (error) {
+    logger.error(error.message, { error });
+    throw new Error('Could not initialize the data layer');
+  }
+
+  return knexInstance;
+}
+
+function getKnex() {
+  if (!knexInstance)
+    throw new Error(
+      'Please initialize the data layer before getting the Knex instance'
+    );
+  return knexInstance;
+}
+
+const tables = Object.freeze({
+  transaction: 'transactions',
+  user: 'users',
+  place: 'places',
+});
+
+module.exports = {
+  initializeData,
+  getKnex,
+  tables,
+};
--- src/index.js
+++ src/index.js
@@ -4,41 +4,50 @@ const bodyParser = require('koa-bodyparser');
 const koaCors = require('@koa/cors');
 const { initializeLogger, getLogger } = require('./core/logging');
 const installRest = require('./rest');
+const { initializeData } = require('./data');
 
 const NODE_ENV = config.get('env');
 const LOG_LEVEL = config.get('log.level');
 const LOG_DISABLED = config.get('log.disabled');
 const CORS_ORIGINS = config.get('cors.origins');
 const CORS_MAX_AGE = config.get('cors.maxAge');
+const dbclient = config.get('database.client');
+console.log('db client ' + dbclient);
 
-initializeLogger({
-  level: LOG_LEVEL,
-  disabled: LOG_DISABLED,
-  defaultMeta: {
-    NODE_ENV,
-  },
-});
-
-const app = new Koa();
-
-app.use(
-  koaCors({
-    origin: (ctx) => {
-      if (CORS_ORIGINS.indexOf(ctx.request.header.origin) !== -1) {
-        return ctx.request.header.origin;
-      }
-      // Not a valid domain at this point, let's return the first valid as we should return a string
-      return CORS_ORIGINS[0];
+async function main() {
+  initializeLogger({
+    level: LOG_LEVEL,
+    disabled: LOG_DISABLED,
+    defaultMeta: {
+      NODE_ENV,
     },
-    allowHeaders: ['Accept', 'Content-Type', 'Authorization'],
-    maxAge: CORS_MAX_AGE,
-  })
-);
+  });
 
-app.use(bodyParser());
+  await initializeData();
 
-installRest(app);
+  const app = new Koa();
 
-app.listen(9000, () => {
-  getLogger().info('🚀 Server listening on http://localhost:9000');
-});
+  app.use(
+    koaCors({
+      origin: (ctx) => {
+        if (CORS_ORIGINS.indexOf(ctx.request.header.origin) !== -1) {
+          return ctx.request.header.origin;
+        }
+        // Not a valid domain at this point, let's return the first valid as we should return a string
+        return CORS_ORIGINS[0];
+      },
+      allowHeaders: ['Accept', 'Content-Type', 'Authorization'],
+      maxAge: CORS_MAX_AGE,
+    })
+  );
+
+  app.use(bodyParser());
+
+  installRest(app);
+
+  app.listen(9000, () => {
+    getLogger().info('🚀 Server listening on http://localhost:9000');
+  });
+}
+
+main();
