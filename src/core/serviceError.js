7ea94a52d86afde8804024888e7d477ae3047081
Thomas Aelbrecht <thomasaelbrecht@live.com>
2023-10-12 14:05:50 +0200

    Add ServiceError

---
+++ src/core/serviceError.js
@@ -0,0 +1,30 @@
+const NOT_FOUND = 'NOT_FOUND';
+const VALIDATION_FAILED = 'VALIDATION_FAILED';
+
+class ServiceError extends Error {
+
+  constructor(code, message, details = {}) {
+    super(message);
+    this.code = code;
+    this.details = details;
+    this.name = 'ServiceError';
+  }
+
+  static notFound(message, details) {
+    return new ServiceError(NOT_FOUND, message, details);
+  }
+
+  static validationFailed(message, details) {
+    return new ServiceError(VALIDATION_FAILED, message, details);
+  }
+
+  get isNotFound() {
+    return this.code === NOT_FOUND;
+  }
+
+  get isValidationFailed() {
+    return this.code === VALIDATION_FAILED;
+  }
+}
+
+module.exports = ServiceError;
