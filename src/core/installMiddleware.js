48ec4c637c4016344c76510a29d93b4110b3ef10
Thomas Aelbrecht <thomasaelbrecht@live.com>
2023-10-12 14:01:05 +0200

    Add request logging

--- src/core/installMiddleware.js
+++ src/core/installMiddleware.js
@@ -1,6 +1,8 @@
 const config = require('config');
 const bodyParser = require('koa-bodyparser');
 const koaCors = require('@koa/cors');
+const emoji = require('node-emoji');
+const { getLogger } = require('./logging');
 
 const CORS_ORIGINS = config.get('cors.origins');
 const CORS_MAX_AGE = config.get('cors.maxAge');
@@ -24,6 +26,35 @@ module.exports = function installMiddleware(app) {
       maxAge: CORS_MAX_AGE,
     })
   );
+
+  app.use(async (ctx, next) => {
+    getLogger().info(`${emoji.get('fast_forward')} ${ctx.method} ${ctx.url}`);
+
+    const getStatusEmoji = () => {
+      if (ctx.status >= 500) return emoji.get('skull');
+      if (ctx.status >= 400) return emoji.get('x');
+      if (ctx.status >= 300) return emoji.get('rocket');
+      if (ctx.status >= 200) return emoji.get('white_check_mark');
+      return emoji.get('rewind');
+    };
+
+    try {
+      await next();
+
+      getLogger().info(
+        `${getStatusEmoji()} ${ctx.method} ${ctx.status} ${ctx.url}`
+      );
+    } catch (error) {
+      getLogger().error(
+        `${emoji.get('x')} ${ctx.method} ${ctx.status} ${ctx.url}`,
+        {
+          error,
+        }
+      );
+
+      throw error;
+    }
+  });
 
   app.use(bodyParser());
 };
