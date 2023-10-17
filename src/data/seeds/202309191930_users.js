ff051598384bc98c9963c65306b19191c5632875
Thomas Aelbrecht <thomasaelbrecht@live.com>
2023-10-17 15:21:10 +0200

    Add user migration and new seed

--- src/data/seeds/202309191930_users.js
+++ src/data/seeds/202309191930_users.js
@@ -10,14 +10,26 @@ module.exports = {
       {
         id: 1,
         name: 'Thomas Aelbrecht',
+        email: 'thomas.aelbrecht@hogent.be',
+        password_hash:
+          '$argon2id$v=19$m=131072,t=6,p=1$9AMcua9h7va8aUQSEgH/TA$TUFuJ6VPngyGThMBVo3ONOZ5xYfee9J1eNMcA5bSpq4',
+        roles: JSON.stringify(['user', 'admin']),
       },
       {
         id: 2,
         name: 'Pieter Van Der Helst',
+        email: 'pieter.vanderhelst@hogent.be',
+        password_hash:
+          '$argon2id$v=19$m=131072,t=6,p=1$9AMcua9h7va8aUQSEgH/TA$TUFuJ6VPngyGThMBVo3ONOZ5xYfee9J1eNMcA5bSpq4',
+        roles: JSON.stringify(['user']),
       },
       {
         id: 3,
         name: 'Karine Samyn',
+        email: 'karine.samyn@hogent.be',
+        password_hash:
+          '$argon2id$v=19$m=131072,t=6,p=1$9AMcua9h7va8aUQSEgH/TA$TUFuJ6VPngyGThMBVo3ONOZ5xYfee9J1eNMcA5bSpq4',
+        roles: JSON.stringify(['user']),
       },
     ]);
   },
