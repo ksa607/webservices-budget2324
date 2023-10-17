869de01c42effa375fe729604b7203ecbc57c31d
Thomas Aelbrecht <thomasaelbrecht@live.com>
2023-10-17 15:19:03 +0200

    Add argon2 with configuration

--- config/development.js
+++ config/development.js
@@ -15,4 +15,12 @@ module.exports = {
     username: 'root',
     password: '',
   },
+  auth: {
+    argon: {
+      saltLength: 16,
+      hashLength: 32,
+      timeCost: 6,
+      memoryCost: 2 ** 17,
+    },
+  },
 };
--- config/production.js
+++ config/production.js
@@ -7,4 +7,12 @@ module.exports = {
     origins: ['http://localhost:5173'],
     maxAge: 3 * 60 * 60,
   },
+  auth: {
+    argon: {
+      saltLength: 16,
+      hashLength: 32,
+      timeCost: 6,
+      memoryCost: 2 ** 17,
+    },
+  },
 };
