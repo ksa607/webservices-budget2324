f8126a105c4ada90b216dbbcac4477a1d5eb130b
Thomas Aelbrecht <thomasaelbrecht@live.com>
2023-10-18 16:53:44 +0200

    Add global setup, teardown and supertest setup

---
+++ __tests__/supertest.setup.js
@@ -0,0 +1,52 @@
+const supertest = require('supertest');
+const createServer = require('../src/createServer');
+const { getKnex } = require('../src/data');
+
+const login = async (supertest) => {
+  const response = await supertest.post('/api/users/login').send({
+    email: 'test.user@hogent.be',
+    password: '12345678',
+  });
+
+  if (response.statusCode !== 200) {
+    throw new Error(response.body.message || 'Unknown error occured');
+  }
+
+  return `Bearer ${response.body.token}`;
+};
+
+const loginAdmin = async (supertest) => {
+  const response = await supertest.post('/api/users/login').send({
+    email: 'admin.user@hogent.be',
+    password: '12345678',
+  });
+
+  if (response.statusCode !== 200) {
+    throw new Error(response.body.message || 'Unknown error occured');
+  }
+
+  return `Bearer ${response.body.token}`;
+};
+
+const withServer = (setter) => {
+  let server;
+
+  beforeAll(async () => {
+    server = await createServer();
+
+    setter({
+      knex: getKnex(),
+      supertest: supertest(server.getApp().callback()),
+    });
+  });
+
+  afterAll(async () => {
+    await server.stop();
+  });
+};
+
+module.exports = {
+  login,
+  loginAdmin,
+  withServer,
+};
