9385b90ed0483a43c05a146b195c5432ff65b06b
Thomas Aelbrecht <thomasaelbrecht@live.com>
2023-10-21 17:23:58 +0200

    Make API ready for deployment

--- config/production.js
+++ config/production.js
@@ -7,6 +7,12 @@ module.exports = {
     origins: ['http://localhost:5173'],
     maxAge: 3 * 60 * 60,
   },
+  database: {
+    client: 'mysql2',
+    host: 'localhost',
+    port: 3306,
+    name: 'budget',
+  },
   auth: {
     argon: {
       saltLength: 16,
